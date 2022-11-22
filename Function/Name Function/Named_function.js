cfec7cba4f067922a742cd9e39a805d5ef1f6815
kaushik134 <kaushikvaghasiya134@gmail.com>
2022-11-22 18:30:11 +0530

    name_function

---
+++ Function/Name Function/Named_function.js
@@ -0,0 +1,53 @@
+
+// function fun_one(){
+//     return "hello";
+// }
+// console.log(fun_one());
+
+
+// function fun_one(){
+//     return "hello";
+// }
+// function fun_two(){
+//     return fun_one();
+// }
+// console.log(fun_two());
+
+
+// function fun_one(){
+//     return "hello";
+// }
+// function fun_two(){
+//     console.log("how are you");
+//     return fun_one();
+// }
+// function fun_three(){
+//     return fun_two();
+// }
+// console.log(fun_three());
+// fun_three();
+
+
+// function fun_one(){
+//     return "hello";
+// }
+// function fun_two(){
+//     return fun_one;
+// }
+// function fun_three(){
+//     return fun_two;
+// }
+// console.log(fun_three()()());
+
+
+// function fun_one(){
+//     return "Hello";
+//     console.log(arg1,arg2)
+// }
+// function fun_two(){
+//     return fun_one();
+// }
+// function fun_three(){
+//     return fun_two();
+// }
+// console.log(fun_three()," ",fun_two());
